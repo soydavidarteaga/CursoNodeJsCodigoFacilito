21fa8934e343f3bf9d4198e03bdc95608a15c695
David Arteaga <david17art@gmail.com>
2016-10-23 02:38:39 -0400

    Instalacion de Body-Parser
    Se instalo body parser para manejar el envio de parametros

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine","jade"); //Motor de vista
 /*Middlewares*/
 	/*Servir archivos*/
 		app.use("/estatico",express.static('public'));
+		app.use(bodyParser.json()); //Para peticiones json
+		app.use(bodyParser.urlencoded({extended:true})); 
 	/*Fin servir artchivos*/
 /*fin middlewares
 
@@ -16,6 +19,11 @@ app.set("view engine","jade"); //Motor de vista
 	});
 	app.get("/login",function(res,res){
 		res.render("login");
+	});
+	app.post("/users",function(req,res){
+		console.log("Contraseña:"+req.body.password);
+		console.log("Email:"+req.body.email);
+		res.send("Recibimos tus datos");
 	});
 /*Fin RUTAS */
 
