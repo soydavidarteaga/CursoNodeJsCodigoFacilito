bac4522c6a943c161699bf8276662e14871189b1
David Arteaga <david17art@gmail.com>
2016-10-23 16:04:25 -0400

    Instalacion de Mongoose y prueba del mismo
    Aqui instalamos Moongose e hicimos una pequeña prueba

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require("express");
 
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 app.set("view engine","jade"); //Motor de vista
+
 /*Middlewares*/
 	/*Servir archivos*/
 		app.use("/estatico",express.static('public'));
@@ -12,18 +15,34 @@ app.set("view engine","jade"); //Motor de vista
 	/*Fin servir artchivos*/
 /*fin middlewares
 
+/*MODELOS Y ESQUEMAS */
+	mongoose.connect("mongodb://localhost/fotos");
+	var userSchemaJson = {
+		email:String,
+		password:String
+	};
+	var user_schema = new Schema(userSchemaJson);
+
+	var User = mongoose.model("User",user_schema);
+/*Fin Modelos*/
 
 /*RUTAS */
 	app.get("/",function(req,res){
 		res.render("index");
 	});
 	app.get("/login",function(res,res){
-		res.render("login");
+		User.find(function(err,doc){
+			console.log(doc);
+			res.render("login");
+		});
+		
 	});
 	app.post("/users",function(req,res){
-		console.log("Contraseña:"+req.body.password);
-		console.log("Email:"+req.body.email);
-		res.send("Recibimos tus datos");
+		var user = new User({email: req.body.email, password: req.body.password}); //Se crera un nuevo usuario
+		user.save(function(){ //Se guarda y asi mismo se envia un callback
+			res.send("Guardamos tus datos");
+		});
+		
 	});
 /*Fin RUTAS */
 
